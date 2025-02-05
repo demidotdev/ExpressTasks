453955b4e2422e516b8b735ab48275a017de69a5
Demian Ovejero <demian07@gmail.com>
2025-02-05 07:36:06 -0300

    Limpiando los middlewares

--- middlewares/find_user.js
+++ middlewares/find_user.js
@@ -1,9 +1,7 @@
 const User = require("../models").User;
 
 module.exports = (req, res, next) => {
-    if (!req.session.userId) return next(); //Se introduce esta linea
-    // fuera de la respuesta asincrona para que no se ejecute la promesa 
-    //si no se ha iniciado sesión.
+    if (!req.session.userId) return next(); 
     User.findByPk(req.session.userId, { include: [
         {
         association: "tasks" // Eager Loading
