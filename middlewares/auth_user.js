9d91d930faa9d98e55594a60ef3b509eab3e788c
Demian Ovejero <demian07@gmail.com>
2025-02-08 09:06:31 -0300

    documentando el archivo auth_user.js

--- middlewares/auth_user.js
+++ middlewares/auth_user.js
@@ -1,6 +1,6 @@
-module.exports = (req, res, next) => {
-    if(!req.originalUrl.includes("tasks")) return next();
-    if(req.session.userId) return next();
+module.exports = (req, res, next) => { // Middleware para verificar si el usuario está loggeado
+    if(!req.originalUrl.includes("tasks")) return next(); // Si la URL no incluye "tasks" no se ejecuta el middleware
+if(req.session.userId) return next(); // Si el usuario está loggeado, se ejecuta el siguiente middleware
 
     res.redirect("/sessions")
 }
