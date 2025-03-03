652f3fc806a82d2e0f618263e051b113c5ed9e54
Demian Ovejero <demian07@gmail.com>
2025-03-03 20:13:35 -0300

    configuring deployment 2

--- realtime/client.js
+++ realtime/client.js
@@ -1,7 +1,7 @@
 //Código para conectar el cliente con el servidor de socket.io y exportar el módulo
 const io = require("socket.io-client");
 
-let host = "http://localhost:8080";
+let host = "http://localhost:5432";
 
 if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
   host = "postgres.railway.internal";
