ef1d3cb1211518decc896e336b35b794f6750a86
Demian Ovejero <demian07@gmail.com>
2025-03-10 21:09:12 -0300

    configuring deployment on render

--- realtime/client.js
+++ realtime/client.js
@@ -4,7 +4,7 @@ const io = require("socket.io-client");
 let host = "http://localhost:8080 ";
 
 if (process.env.NODE_ENV && process.env.NODE_ENV == "production") {
-  host = process.env.HOST;
+  host = process.env.host;
 }
 
 let socket = io.connect(host, { reconnect: true });
