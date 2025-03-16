c85518b7855a4131f4c3a2094ea8eade587dd4db
Demian Ovejero <demian07@gmail.com>
2025-03-16 09:14:07 -0300

    configuring deployment on render

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const sessionsRoutes = require("./routes/sessions_routes"); // Para manejar las 
 const categoriesRoutes = require("./routes/categories_routes"); // Para manejar las rutas de las categorias
 const findUserMiddleware = require("./middlewares/find_user"); // Para mostrar el ususario loggeado en el home
 const authUserMiddeleware = require("./middlewares/auth_user");
-import PostgresStore from "@sequelize/postgress";
+const PostgresStore = require("@sequelize/postgres");
 
 //El método "use" inserta un nuevo Middleware en el stack.
 app.use(bodyParser.urlencoded({ extended: true })); // Para tomar la data del body ya formateada
