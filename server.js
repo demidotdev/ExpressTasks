7fcc8a78ea4f15024a649f52f715f8f5e797feef
Demian Ovejero <demian07@gmail.com>
2025-03-17 12:07:04 -0300

    migrating from commonJS to ESmodules

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ import sessionsRoutes from "./routes/sessions_routes.js"; // Para manejar las ru
 import categoriesRoutes from "./routes/categories_routes.js"; // Para manejar las rutas de las categorias
 import findUserMiddleware from "./middlewares/find_user.js"; // Para mostrar el ususario loggeado en el home
 import authUserMiddeleware from "./middlewares/auth_user.js";
-import PostgresStore from "sequelize/postgres";
 
 //El método "use" inserta un nuevo Middleware en el stack.
 app.use(urlencoded({ extended: true })); // Para tomar la data del body ya formateada
