2707fb5ef1f6f1bd4ed29b63818fb81404a37185
Demian Ovejero <demian07@gmail.com>
2025-03-17 21:16:48 -0300

    migrating from commonJS to ESmodules

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ import sessionsRoutes from "./routes/sessions_routes.js"; // Para manejar las ru
 import categoriesRoutes from "./routes/categories_routes.js"; // Para manejar las rutas de las categorias
 import findUserMiddleware from "./middlewares/find_user.js"; // Para mostrar el ususario loggeado en el home
 import authUserMiddeleware from "./middlewares/auth_user.js";
+import connectPgSimple from "connect-pg-simple";
 
 //El método "use" inserta un nuevo Middleware en el stack.
 app.use(urlencoded({ extended: true })); // Para tomar la data del body ya formateada
@@ -32,7 +33,7 @@ let sessionConfig = {
 };
 
 if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
-  sessionConfig["store"] = new (import("connect-pg-simple")(session))(); // Para guardar las sesiones en la base de datos
+  sessionConfig["store"] = new (connectPgSimple(session))(); // Para guardar las sesiones en la base de datos
 }
 app.use(session(sessionConfig));
 
