9db3ea08fab4bb8a2c1a82c82108b2704055789d
Demian Ovejero <demian07@gmail.com>
2025-04-22 22:35:47 -0300

    cambio puerto en el cliente de websockets

--- realtime/client.js
+++ realtime/client.js
@@ -1,10 +1,10 @@
 //Código para conectar el cliente con el servidor de socket.io y exportar el módulo
-const io = require('socket.io-client');
+const io = require("socket.io-client");
 
-let socket = io.connect('http://localhost:3000', {reconnect: true});
+let socket = io.connect("http://localhost:8080", { reconnect: true });
 
-socket.on('connect',function(){  
-    console.log("\n\nSocket connected from NodeJS\n\n")
-})
+socket.on("connect", function () {
+  console.log("\n\nSocket connected from NodeJS\n\n");
+});
 
 module.exports = socket;
